bb17056e493a30510360a7eb41d2295a94368a76
Omid Bodaghi <42227752+omibo@users.noreply.github.com>
2026-03-09 12:18:08 -0700

    Use constants in tests/commitment

--- tests/commitment/mod.rs
+++ tests/commitment/mod.rs
@@ -10,6 +10,7 @@ use hachi_pcs::{CanonicalField, FieldCore, FromSmallInt};
 
 const WITNESS_LEN: usize = 40;
 const NUM_WITNESS_ROWS: usize = 12;
+const TEST_SALT_BASE: usize = 11;
 
 fn test_config() -> CoeffAjtaiConfig {
     CoeffAjtaiConfig {
@@ -48,11 +49,11 @@ fn build_slot<F: FieldCore + CanonicalField, const D: usize>(
 
 fn assert_ntt_matches_coeff<F: FieldCore + CanonicalField + FromSmallInt, const D: usize>() {
     let cfg = test_config();
-    let witness = sample_small_instances::<F, D>(NUM_WITNESS_ROWS, WITNESS_LEN, 11);
-    let matrix_a = sample_small_instances::<F, D>(cfg.inner_rows, WITNESS_LEN, 12);
+    let witness = sample_small_instances::<F, D>(NUM_WITNESS_ROWS, WITNESS_LEN, TEST_SALT_BASE);
+    let matrix_a = sample_small_instances::<F, D>(cfg.inner_rows, WITNESS_LEN, TEST_SALT_BASE + 1);
 
     let decomp_len = NUM_WITNESS_ROWS * cfg.inner_rows * cfg.num_digits;
-    let matrix_b = sample_small_instances::<F, D>(cfg.outer_rows, decomp_len, 13);
+    let matrix_b = sample_small_instances::<F, D>(cfg.outer_rows, decomp_len, TEST_SALT_BASE + 2);
 
     let a_ntt = build_slot::<F, D>(&matrix_a);
     let b_ntt = build_slot::<F, D>(&matrix_b);
