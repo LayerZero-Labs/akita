fad006e2280e880fa16f1cd13b5ea2df599364d0
Quang Dao <qvd@andrew.cmu.edu>
2026-07-20 02:31:26 +0530

    fix(fuzz): use segment-typed witness shape

--- fuzz/fuzz_targets/proof_shapes.rs
+++ fuzz/fuzz_targets/proof_shapes.rs
@@ -2,12 +2,12 @@
 
 use akita_serialization::AkitaDeserialize;
 use akita_types::{
-    AkitaBatchedProofShape, CleartextWitnessShape, LevelProofShape, TerminalLevelProofShape,
+    AkitaBatchedProofShape, LevelProofShape, SegmentTypedWitnessShape, TerminalLevelProofShape,
 };
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = CleartextWitnessShape::deserialize_compressed(data, &());
+    let _ = SegmentTypedWitnessShape::deserialize_compressed(data, &());
     let _ = LevelProofShape::deserialize_compressed(data, &());
     let _ = TerminalLevelProofShape::deserialize_compressed(data, &());
     let _ = AkitaBatchedProofShape::deserialize_compressed(data, &());
